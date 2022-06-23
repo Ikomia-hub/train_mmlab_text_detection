951b7f9ced4578f0692fdfb1f23b6e34db781ced
ambroise <ambroise.berthe@ikomia.com>
2022-06-23 15:02:59 +0200

    Update test to test only one config by model

--- train_mmlab_text_detection_test.py
+++ train_mmlab_text_detection_test.py
@@ -15,6 +15,7 @@ def test(t, data_dict):
     plugins_folder = ikomia.ik_registry.getPluginsDirectory()
     plugin_folder = os.path.join(plugins_folder, "Python", t.name)
     configs_path = os.path.join(plugin_folder, "configs", "textdet")
+    input_dataset.load(data_dict["datasets"]["text"]["dataset_wildreceipt"])
     # loop on every configs available
     for model_name in os.listdir(configs_path):
         if os.path.isdir(os.path.join(configs_path, model_name)) and model_name != "_base_":
@@ -22,17 +23,15 @@ def test(t, data_dict):
             if os.path.isfile(yaml_file):
                 with open(yaml_file, "r") as f:
                     models_list = yaml.load(f, Loader=yaml.FullLoader)['Models']
-                for model_dict in models_list:
-                    cfg = os.path.basename(model_dict["Config"])
-                    ckpt = model_dict["Weights"]
-                    params["cfg"] = cfg
-                    params["weights"] = ckpt
-                    params["model_name"] = model_name
-                    params["pretrain"] = True
-                    params["epochs"] = 2
-                    params["batch_size"] = 1
-                    params["eval_period"] = 1
-
-                    task.set_parameters(t, params)
-                    input_dataset.load(data_dict["datasets"]["text"]["dataset_wildreceipt"])
-                    yield run_for_test(t)
+                model_dict = models_list[0]
+                cfg = os.path.basename(model_dict["Config"])
+                ckpt = model_dict["Weights"]
+                params["cfg"] = cfg
+                params["weights"] = ckpt
+                params["model_name"] = model_name
+                params["pretrain"] = True
+                params["epochs"] = 2
+                params["batch_size"] = 1
+                params["eval_period"] = 1
+                task.set_parameters(t, params)
+                yield run_for_test(t)
